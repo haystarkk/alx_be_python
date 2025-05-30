c829afb577a2c51c45e0d1b2eef475fcf2730000
Michael <165153585+haystarkk@users.noreply.github.com>
2025-05-30 16:41:04 +0300

    Create match_case_calculator.py

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,22 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the secodf number: "))
+sign = input("Choose the operation (+, -, *, /): ")
+
+match sign:
+    case '+':
+        result = num1 + num2
+        print(f"The result is {result}.")
+    case '-':
+        result = num1 - num2
+        print(f"The result is {result}.")
+    case '*':
+        result = num1 * num2
+        print(f"The result is {result}.")
+    case '/':
+        if num2 != 0:
+            result = num1 / num2
+            print(f"The result is {result}.")
+        else:
+            print("Cannot divide by zero.")
+    case _: 
+        print("Invalid operation. Please choose from +, -, *, /.")
