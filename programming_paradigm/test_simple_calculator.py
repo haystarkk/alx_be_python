5dafd2afa3fea575c45ac837ebccf0843acad36e
Michael <165153585+haystarkk@users.noreply.github.com>
2025-07-28 07:27:32 +0300

    Update test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -3,18 +3,30 @@ from simple_calculator import SimpleCalculator
 
 class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
-        """Set up a SimpleCalculator instance before each test."""
+        """Create a SimpleCalculator instance before each test."""
         self.calc = SimpleCalculator()
 
+    def test_addition(self):
+        """Test addition method."""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(0, 0), 0)
+
+    def test_subtraction(self):
+        """Test subtraction method."""
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(3, 5), -2)
+        self.assertEqual(self.calc.subtract(-5, -5), 0)
+
     def test_multiply(self):
-        """Test multiplication operation."""
+        """Test multiplication method."""
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(-2, 3), -6)
         self.assertEqual(self.calc.multiply(-2, -3), 6)
-        self.assertEqual(self.calc.multiply(0, 100), 0)
+        self.assertEqual(self.calc.multiply(0, 5), 0)
 
     def test_divide(self):
-        """Test division operation."""
+        """Test division method."""
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(-9, 3), -3)
         self.assertEqual(self.calc.divide(5, 2), 2.5)
